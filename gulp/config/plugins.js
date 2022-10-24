8d7fc4ac85937552c54c975cc6b7a21cc4c70c99
Denys Bilyk <bilko.dp@gmail.com>
2022-10-24 19:36:16 +0300

    first comit

---
+++ gulp/config/plugins.js
@@ -0,0 +1,16 @@
+import replace from "gulp-replace"; //Поиск и замена
+import plumber from "gulp-plumber"; //Обработка ошибок
+import notify from 'gulp-notify'; //Сообщения (подсказки)
+import browserSync from "browser-sync"; //Локальный сервер
+import newer from "gulp-newer"; //Проверка обновления 
+import gulpIf from 'gulp-if'; //Условное ветвление
+
+//Экспортируем объект
+export const plugins = {
+	replace,
+	plumber,
+	notify,
+	browserSync,
+	newer,
+	if: gulpIf
+};
